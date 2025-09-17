9c6a2c72cc95606b7f7d4f9f046364dd86fc2e63
KevinThoriya <kevinthoriya0074@gmail.com>
2025-09-17 20:55:39 +0530

    build: bump version to 0.2.16

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omnidimension",
-    version="0.2.15",
+    version="0.2.16",
     packages=find_packages() + ["omnidim_mcp_server"],
     install_requires=["requests"],
     extras_require={

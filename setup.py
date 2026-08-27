8e7965807a0ebf45cebf883e4dc967ff6cdb9a98
Alok Ranjan <alok@omnidim.io>
2026-08-28 02:51:56 +0530

    chore: release 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omnidimension",
-    version="0.2.19",
+    version="0.3.0",
     packages=find_packages() + ["omnidim_mcp_server"],
     install_requires=["requests"],
     extras_require={
